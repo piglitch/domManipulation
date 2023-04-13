84e440b2d267d7764168ccae93a1465fc0663b7b
Avi <avibanerjee2000@gmail.com>
2023-04-13 16:22:41 +0530

    sibling traversal

--- traverse.js
+++ traverse.js
@@ -4,4 +4,29 @@
 
 //Parent Node Traversal
 
-let ul = document.query
+
+//console.log(ul.parentNode);
+//console.log(ul.parentElement.parentElement);
+
+// Child node traversal
+
+//let ul = document.querySelector('ul');
+//console.log(ul.childNodes);
+//console.log(ul.firstChild);
+//console.log(ul.lastChild);
+
+//ul.childNodes[1].style.backgroundColor = 'blue';
+
+
+//console.log(ul.children);
+//console.log(ul.firstElementChild);
+//console.log(ul.lastElementChild);
+
+//Sibling traversal 
+
+let ul = document.querySelector('ul');
+const div = document.querySelector('div')
+
+console.log(div.childNodes);
+console.log(ul.previousSibling);
+console.log(ul.nextSibling);
