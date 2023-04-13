1d6a7e152e1734f942646b9cbe2d831dea23cded
Avi <avibanerjee2000@gmail.com>
2023-04-13 17:05:41 +0530

    .

--- eventListeners/app.js
+++ eventListeners/app.js
@@ -4,3 +4,10 @@
 
 //element.addEventListener("click", function);
 
+const buttonTwo = document.querySelector('.btn-2');
+
+function alertBtn(){
+    alert('Yo!');
+}
+
+buttonTwo.addEventListener("click", alertBtn);
