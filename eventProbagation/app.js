4d7a9ed1188840b8557a126ca8569f62531af653
piglitch <73684837+piglitch@users.noreply.github.com>
2023-04-18 19:04:54 +0530

    added js

---
+++ eventProbagation/app.js
@@ -0,0 +1,23 @@
+// Dom manipulation
+
+// Event Probagation
+
+window.addEventListener("click", function(){
+    console.log('window');
+}, true);
+
+document.addEventListener("click", function(){
+    console.log('Document');
+}, true);
+
+document.querySelector(".div2").addEventListener("click", function(){
+    console.log('DIV 2');
+}, true);
+
+document.querySelector(".div1").addEventListener("click", function(){
+    console.log('DIV 1');
+}, true);
+
+document.querySelector("button").addEventListener("click", function(e){
+    console.log(e);
+}, true);
