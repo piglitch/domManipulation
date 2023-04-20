df41fe34858715bae2293c05a54feaa8d3a4a14c
Avi <avibanerjee2000@gmail.com>
2023-04-20 20:45:34 +0530

    .

---
+++ EventDelegation/app.js
@@ -0,0 +1,52 @@
+// Dom mainipulation
+
+// Event delegation 
+
+// Allows users to append a single event listener to a parent element rgar adds it to all of its present
+// present and future descendants that match a selector.
+
+document.querySelector('#football').addEventListener('click', function(e){
+    console.log('football is clicked');
+
+    const target  = e.target;
+    if (target.matches('li')){
+        target.style.backgroundColor = 'lightgrey';
+    }
+})
+
+document.querySelector('#basketball').addEventListener('click', function(e){
+    console.log('basketball is clicked');
+
+    const target  = e.target;
+    if (target.matches('li')){
+        target.style.backgroundColor = 'lightgrey';
+    }
+})
+
+document.querySelector('#tennis').addEventListener('click', function(e){
+    console.log('tennis is clicked');
+
+    const target  = e.target;
+    if (target.matches('li')){
+        target.style.backgroundColor = 'lightgrey';
+    }
+})
+
+document.querySelector('#cricket').addEventListener('click', function(e){
+    console.log('cricket is clicked');
+
+    const target  = e.target;
+    if (target.matches('li')){
+        target.style.backgroundColor = 'lightgrey';
+    }
+})
+
+document.querySelector('#boxing').addEventListener('click', function(e){
+    console.log('boxing is clicked');
+
+    const target  = e.target;
+    if (target.matches('li')){
+        target.style.backgroundColor = 'lightgrey';
+    }
+})
+
